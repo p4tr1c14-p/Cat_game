0fdb333506a4988341e6755b14aa1e9e94963c40
patricia <patriciaperezcruz007@gmail.com>
2025-05-13 16:38:20 -0600

    version 4

---
+++ Version_0_4/Cat_game.py
@@ -0,0 +1,40 @@
+"""
+Nombre:
+Fecha: 12 de mayo del 2025.
+
+Descripción:
+"""
+import pygame
+
+
+from Configurations import Configurations
+from Game_functionalities import game_event, screen_refresh
+from Media import Background
+
+def run_game() -> None:
+    """
+    Función principal del videojuego
+    """
+    #Se inicializa el módulo pygame
+    pygame.init()
+
+    clock = pygame.time.Clock()
+
+    #Se inicializa la pantalla
+    #screen_size = (1280, 720) #Resolución de la pantalla (ancho, alto)
+    screen = pygame.display.set_mode(Configurations.get_screen_size() )
+
+    #Se configura el título del juego
+    #game_title = "Snake game en pygame"
+    pygame.display.set_caption(Configurations.get_game_title())
+
+    #Ciclo principal de videojuego
+    game_over = False
+    background = Background()
+
+    while not game_over:
+        game_over = game_event()
+        screen_refresh(screen, clock, background)
+
+if __name__ == '__main__':
+    run_game()
---
+++ Version_0_4/Media.py
@@ -0,0 +1,56 @@
+import pygame
+from Configurations import Configurations
+
+
+class Background:
+    """
+    Clase que contiene el fondo de pantalla
+    """
+
+    def __init__(self):
+        background_image_path = Configurations.get_background_image_path()
+        self.image = pygame.image.load(background_image_path)
+
+        # Se escala la imagen al tamaño de la pantalla
+        screen_size = Configurations.get_screen_size()
+        self.image = pygame.transform.scale(self.image, screen_size)
+
+        self.rect = self.image.get_rect()
+
+    def blit(self, screen: pygame.surface.Surface):
+        """
+        Se utiliza para dibujar el fondo de pantalla
+        """
+        screen.blit(self.image, self.rect)
+
+class Mark_X:
+    def __init__(self):
+
+        x_imagex_path = Configurations.get_x_images_path()
+        self.imagex = pygame.image.load(x_imagex_path)
+
+        self.imagex = pygame.image.load(Configurations.get_x_images_path())
+        self.rect = self.imagex.get_rect()
+
+    def blit(self, screen: pygame.surface.Surface) -> None:
+        """
+        Se utiliza para dibujar la manzana
+        :param screen: Pantalla en donde se dibuja
+        """
+        screen.blit(self.imagex, self.rect)
+
+class Mark_O:
+    def __init__(self):
+
+        o_image_path = Configurations.get_o_images_path()
+        self.image = pygame.image.load(o_image_path)
+
+        self.image = pygame.image.load(Configurations.get_o_images_path())
+        self.rect = self.image.get_rect()
+
+    def blit(self, screen: pygame.surface.Surface) -> None:
+        """
+        Se utiliza para dibujar la manzana
+        :param screen: Pantalla en donde se dibuja
+        """
+        screen.blit(self.image, self.rect)
